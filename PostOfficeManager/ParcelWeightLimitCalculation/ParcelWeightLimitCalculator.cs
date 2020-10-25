7bd154eec8184192b020021ab59a120b03f0bdf5
Artem Shaitarov <Artem.Shaitarov@vista.co>
2020-10-26 12:53:17 +1300

    Add an implementation for a parcel weight limit calculator.

--- PostOfficeManager/ParcelWeightLimitCalculation/ParcelWeightLimitCalculator.cs
+++ PostOfficeManager/ParcelWeightLimitCalculation/ParcelWeightLimitCalculator.cs
@@ -1,12 +1,18 @@
-﻿using PostOfficeManager.Models;
+﻿using System;
+using PostOfficeManager.Models;
 
 namespace PostOfficeManager.ParcelWeightLimitCalculation
 {
     public class ParcelWeightLimitCalculator : IParcelWeightLimitCalculator
     {
-        public int CalculateWeightLimit(ParcelSizeFactor sizeFactor)
-        {
-            throw new System.NotImplementedException();
-        }
+        public int CalculateWeightLimit(ParcelSizeFactor sizeFactor) =>
+            sizeFactor switch
+            {
+                ParcelSizeFactor.Small => 1,
+                ParcelSizeFactor.Medium => 3,
+                ParcelSizeFactor.Large => 6,
+                ParcelSizeFactor.ExtraLarge => 10,
+                _ => throw new NotImplementedException()
+            };
     }
 }
